86548bb808bcb98310e471f805b42a3e6d074008
jaewonac <jaewon.a.c@gmail.com>
2020-04-07 04:34:22 +0000

    fixture data model default value

--- jalabel_site/jalabel_app/models.py
+++ jalabel_site/jalabel_app/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 class FixtureData(models.Model):
     lot_number = models.CharField(max_length=20)
     image_data = thumbnail.ImageField(upload_to='fixture/', blank=True)
-    message = models.CharField(max_length=20)
+    message = models.CharField(max_length=20, default='unknown')
     uploaded_date = models.DateTimeField(unique=True)
     last_modified = models.DateTimeField(auto_now=True)
     rle_csv = models.ImageField(upload_to='fixture_rle/', blank=True)
